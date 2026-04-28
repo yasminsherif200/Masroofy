43a6c240b73f5850fe2dc2b15c9caf1835089372
yasmin <yasminsherif2028@gmail.com>
2026-04-28 16:42:05 +0300

    Edited urls + runserver

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', login_view.login, name='login'),
-    path('/signup/', login_view.signup, name='signup'),
-    path('/logout/', login_view.logout, name='logout'),
+    path('signup/', login_view.signup, name='signup'),
+    path('logout/', login_view.logout, name='logout'),
 ]
