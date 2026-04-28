ad5a21fba866e30085f335610aff7834395008a7
yasmin <yasminsherif2028@gmail.com>
2026-04-28 16:25:23 +0300

    urls Added

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from core.views import login_view
+from django.urls import path
+
+urlpatterns = [
+    path('', login_view.login, name='login'),
+    path('/signup/', login_view.signup, name='signup'),
+    path('/logout/', login_view.logout, name='logout'),
+]
