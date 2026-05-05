97244198cf4b4d26870d98c5cc105f470346a6a4
yasmin <yasminsherif2028@gmail.com>
2026-05-05 09:32:18 +0300

    Updating dudget_dao

--- core/dao/budget_dao.py
+++ core/dao/budget_dao.py
@@ -13,3 +13,10 @@ class BudgetDAO:
 
     def deleteCycle(self, cycleID):
         BudgetCycle.objects.filter(cycleID=cycleID).delete()
+
+    # get cycle by its ID
+    def getCycleByCycleID(self, cycleID):
+        try:
+            return BudgetCycle.objects.get(cycleID=cycleID)
+        except BudgetCycle.DoesNotExist:
+            return None
